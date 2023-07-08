a935a7d934d913d71fb60c7986dcbd978f04c98e
Valex4 <yam778123@gmail.com>
2023-07-08 15:50:26 -0600

    feat: pruebaEliminacion

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,13 @@ import express from "express";
 import { Signale } from "signale";
 
 import { loadRouter } from "./event/LoadRouter";
-import { productRouter } from "./product/infrastructure/ProductRouter";
 
 const app = express();
 
 const signale = new Signale();
 
 app.use(express.json());
-app.use("/products", productRouter);
+
 app.use("/load", loadRouter);
 
 app.listen(4000, () => {
